c12a1077924a149be70a288f621cb915c54570fb
dpsiu <denversiu24@gmail.com>
2023-07-12 22:25:34 -0500

    Add feature - append, prepend

---
+++ script.js
@@ -0,0 +1,49 @@
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.head = null;
+      this.tail = null;
+    }
+  
+
+    append(value) {
+      const newNode = new Node(value);
+  
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = newNode;
+      } else {
+        this.tail.next = newNode;
+        this.tail = newNode;
+      }
+    }
+  
+    prepend(value){
+        const newNode = new Node(value)
+        if (!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            newNode.next = this.head
+            this.head = newNode
+        }
+    }
+
+    print() {
+      let current = this.head;
+      while (current) {
+        console.log(current.value);
+        current = current.next;
+      }
+    }
+
+
+  }
+  
+  const linkedList = new LinkedList()
